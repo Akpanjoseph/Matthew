9867056f93eb109f67ecfc8afd3d4d75aa07b36f
Syntax Designer <syntaxdesigners@gmail.com>
2023-06-20 16:32:40 +0100

    Finished project setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <img src={reactLogo} alt="React Logo" className='w-[100px]' />
+        <p className='text-red-500'>text</p>
+      </div>
+    </>
+  )
+}
+
+export default App
