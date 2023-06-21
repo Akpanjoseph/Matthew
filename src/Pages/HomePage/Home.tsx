05d6ff535c0f1f74ff66cf81f8b50931dad517d5
Syntax Designer <syntaxdesigners@gmail.com>
2023-06-21 13:43:04 +0100

    finished with feature section

--- src/Pages/HomePage/Home.tsx
+++ src/Pages/HomePage/Home.tsx
@@ -1,10 +1,11 @@
 import Header from "./Components/Header"
+import Features from "./Features"
 
 const Home=()=>{
     return(
        <>
        <Header/>
-        
+        <Features/>
         </>
     )
 }
