1919d2eb8e4afaf49df5baefcb15a40c09865cf8
Syntax Designer <syntaxdesigners@gmail.com>
2023-06-28 10:13:49 +0100

    started working on the dashboard

---
+++ src/Pages/DashBoard/DashBoard.tsx
@@ -0,0 +1,21 @@
+
+import logo from '../../assert/logo.png'
+const DashBoard = ()=>{
+    return(
+        <div className="flex m-0 p-0">
+            <div className="w-[15%] shadow-md bg-gray-50 h-screen"> 
+            <img src={logo} alt="" className='' />
+            </div>
+
+
+            <div className="w-[85%] ">
+                <div className="w-full bg-dark">
+                    <p>dd</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default DashBoard; 
